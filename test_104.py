6f68a83896500f2c2f317c6607d5d459d16bedd9
minglu <individual140@gmail.com>
2017-07-03 00:13:23 +0800

    crawler 104

--- test_104.py
+++ test_104.py
@@ -5,16 +5,16 @@ from collections import Counter
 import time
 cnt=Counter()#比較器
 count=1      #偵錯用
-p=1          #頁數
 try:
+    p=1 #第一頁
     while True:
         set1=set()
         url="https://www.104.com.tw/jobbank/joblist/joblist.cfm?jobsource=n104bank1&ro=0&keyword=%E8%BB%9F%E9%AB%94%E5%B7%A5%E7%A8%8B%E5%B8%AB&order=1&asc=0&page="+str(p)+"&psl=N_A"
-        p+=1
         re =requests.get(url)
         soup=BeautifulSoup(re.text,'lxml')
         #print(soup.select(".job_name > a"))
-        next=soup.select(".mar_L10 ")[0].select("a")[0]
+        next=soup.select(".mar_L10 ")[0]
+        print(next.text)
         for murl in soup.select(".job_name > a"):
             nu=str(murl["href"]).split("jobno=")[1].split("&")[0]
             set1.add(nu)
@@ -40,9 +40,11 @@ try:
                     	print("不拘")
                     	cnt["不拘"]+=1
                	print("=" * 50)
+        if(next.text==""):  ##because 149 also "" so 設150頁也是思考重點
+            if(p==150):
+                break
+        p+=1
         time.sleep(3)
-        if(next.text=="下一頁"):
-            break
     print(cnt.most_common(20))
 except requests.ConnectionError:
         print("Connection aborted")
